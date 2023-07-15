1840fcc4f2f0595112f4a624ffdde667ada99fa7
Luka Ernestini <luka.ernestini@gmail.com>
2023-07-15 21:15:11 +0200

    use controlled elements technique

--- ultimate-react-course-main/05-travel-list/starter/travel-list/src/App.tsx
+++ ultimate-react-course-main/05-travel-list/starter/travel-list/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
@@ -16,15 +17,74 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [selectedValue, setSelectedValue] = useState(1);
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!description) {
+      return;
+    }
+
+    const newItem: ItemData = {
+      description,
+      id: Date.now(),
+      packed: false,
+      quantity: selectedValue,
+    };
+
+    console.log(newItem);
+
+    setDescription("");
+    setSelectedValue(1);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select
+        value={selectedValue}
+        onChange={(e) => setSelectedValue(+e.target.value)}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }: { item: ItemData }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
@@ -36,3 +96,16 @@ function Stats() {
 }
 
 export default App;
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+];
+
+type ItemData = {
+  id: number;
+  description: string;
+  quantity: number;
+  packed: boolean;
+};
