ba21772a682c30be24da527b981ead32dd5187f7
Luka Ernestini <luka.ernestini@gmail.com>
2023-07-16 09:40:15 +0200

    refactor footer

--- ultimate-react-course-main/05-travel-list/starter/travel-list/src/App.tsx
+++ ultimate-react-course-main/05-travel-list/starter/travel-list/src/App.tsx
@@ -151,14 +151,24 @@ function Stats({ items }: { items: ItemData[] }) {
   const packed = items.filter((item) => item.packed).length;
   const packedPercent = Math.round((packed / total) * 100);
 
-  return (
-    <footer className="stats">
+  const renderStats = () => {
+    if (!total) {
+      return <em>Start adding some items to your packing list 🚀</em>;
+    }
+
+    if (packedPercent === 100) {
+      return <em>You got everything! ready to go ✈</em>;
+    }
+
+    return (
       <em>
-        👜 You have {total} items on your list, and you already packed {packed}{" "}
-        ({packedPercent}%)
+        👜 You have {total} items on your list, and you already packed {packed}(
+        {packedPercent}%)
       </em>
-    </footer>
-  );
+    );
+  };
+
+  return <footer className="stats">{renderStats()}</footer>;
 }
 
 const initialItems = [
